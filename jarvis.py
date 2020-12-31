a0510e75ce833065e6eefd56821c46e4e455b2ef
Tushakar <tushakarbhardwaj@gmail.com>
2020-12-31 16:14:15 +0530

    Update jarvis.py
    Adding speak Function

--- jarvis.py
+++ jarvis.py
@@ -1,1 +1,20 @@
 import os
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
+engine.setProperty('voice',voices[0].id)
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+#def wishMe():
+
+
+if __name__ == "__main__":
+    speak("Tushakar is a good boy")
