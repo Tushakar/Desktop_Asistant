5f587b0a92e30c9c8a3fc3e4ea67965f5d076c41
Tushakar <tushakarbhardwaj@gmail.com>
2020-12-31 16:38:52 +0530

    Update jarvis.py
    Adding wish Function

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,11 @@
 import os
 import pyttsx3
+import datetime
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[0].id)
+
 engine.setProperty('voice',voices[0].id)
 
 
@@ -13,8 +15,21 @@ def speak(audio):
     engine.runAndWait()
 
 
-#def wishMe():
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour <12:
+        speak("Good Morning!")
+    
+    elif hour>=12 and hour <18:
+        speak("Good Afternoon!")
 
+    else:
+        speak("Good Evening!")
+
+
+    speak("I am Jarvis Sir.Please tell me how may I help you?")
 
 if __name__ == "__main__":
-    speak("Tushakar is a good boy")
+    wishMe()
+
+
