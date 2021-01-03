c2f1513f083e7a8426154cae4f5830ff66551850
Tushakar <tushakarbhardwaj@gmail.com>
2021-01-03 13:38:25 +0530

    Update jarvis.py
    adding send email functionality

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,12 @@
 import os
-import pyttsx3
 import datetime
-import speech_recognition as sr
-import wikipedia
 import webbrowser
+import smtplib
+import wikipedia
+import pyttsx3
+import speech_recognition as sr
+
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -43,7 +46,7 @@ def takeCommand():
 
     try:
          print("Recognizing...")
-         query = r.recognize_google(audio,Language = 'en-in')
+         query = r.recognize_google(audio,language = 'en-in')
          print(f"User said: {query}\n")
     
     except Exception as e:
@@ -52,45 +55,64 @@ def takeCommand():
         return "None"
     return query
 
+def sendEmail(to,content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+
 
 
 
 
 if __name__ == "__main__":
     wishMe()
-    if 1:
-    query = takeCommand().lower()
+    while True:
+        query = takeCommand().lower()
 
-    if 'wikipedia' in query:
-        speak('Searching Wikipedia...')
-        query = query.replace("wikipedia","")
-        results = wikipedia.summary(query,sentences=2)
-        speak("According to Wikipedia")
-        print(results)
-        speak(results)
-    elif 'open youtube' in query:
-        webbrowser.open("youtube.com")
-        
-    elif 'open google' in query:
-        webbrowser.open("google.com")
-
-    elif 'open stackoverflow' in query:
-        webbrowser.open("stackoverflow.com")
-
-    elif 'play music' in query:
-        music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-        songs = os.listdir(music_dir)
-        print(songs)
-        os.startfile(os.path.join(music_dir, songs[0]))
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
             
-    elif 'the time' in query:
-        strTime  = datetime.datetime.now().strftime("%H:%M:%S")
-        speak(f"Sir , the time is {strTime}")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
 
-    elif 'open code' in query:
-        codepath = "C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe"
-        os.startfile(codepath)
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
 
+        elif 'play music' in query:
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+                
+        elif 'the time' in query:
+            strTime  = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir , the time is {strTime}")
+
+        elif 'open code' in query:
+            codepath = "C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe"
+            os.startfile(codepath)
+
+        elif 'email to manav' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "manavd04@gmail.com"  #Fake email id
+                sendEmail(to,content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend tushakar.I am not able to send this email")
 
 
 
